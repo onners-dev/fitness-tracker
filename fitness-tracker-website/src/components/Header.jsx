f401336396043176d1385ef5466011c53557cb4e
Onni-Petteri Rantanen <onnin2003@gmail.com>
2024-12-01 14:41:34 +0200

    Add protected route, and fix login pages

--- fitness-tracker-website/src/components/Header.jsx
+++ fitness-tracker-website/src/components/Header.jsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './Header.css'
 
 const Header = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem('token');
+
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    navigate('/home');
+  };
+
   return (
     <header className="header">
       <Link to="/home" className="logo">Fitness App</Link>
       <nav>
-        <Link to="/home#features" className="link">Features</Link>
-        <Link to="/about" className="link">About</Link>
-        <Link to="/login" className="link">Login</Link>
+        {isLoggedIn ? (
+          // Links for logged-in users
+          <>
+            <Link to="/dashboard" className="link">Dashboard</Link>
+            <Link to="/about" className="link">About</Link>
+            <button onClick={handleSignOut} className="link">Sign Out</button>
+          </>
+        ) : (
+          // Links for logged-out users
+          <>
+            <Link to="/home#features" className="link">Features</Link>
+            <Link to="/about" className="link">About</Link>
+            <Link to="/login" className="link">Login</Link>
+          </>
+        )}
       </nav>
     </header>
   );
