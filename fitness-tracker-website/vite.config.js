71cbb87169735992409f7eb5e0f01116cb15cbbb
yoinkeds <onnin2003@gmail.com>
2024-12-16 03:06:54 +0000

    Change frontend to fit for server

--- fitness-tracker-website/vite.config.js
+++ fitness-tracker-website/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -12,5 +11,9 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  define: {
+    'process.env': process.env,
   }
 });
+
